41c8728cd98fdb2128f581cd3a968a1365f56aba
JonasKimio <jonaskimio1@hotmail.com>
2024-11-18 20:37:02 -0300

    versão mobile no index

--- js/Dm.js
+++ js/Dm.js
@@ -1,11 +1,24 @@
-function mudarTema(){
+function mudarTema() {
     var body = document.body;
-    if(body.classList.contains("light-theme")){
+    if (body.classList.contains("light-theme")) {
         body.classList.remove("light-theme");
         body.classList.add("dark-theme")
     }
-    else{
+    else {
         body.classList.remove("dark-theme");
         body.classList.add("light-theme")
     }
+}
+
+function menu() {
+    var modal = document.getElementById('contatomodal');
+    modal.style.display = 'block';
+}
+window.onclick = function (event) {
+    const modal = document.getElementById('contatomodal');
+    const modalContent = document.querySelector('.modal-content');
+
+    if (event.target === modal) { // Se o clique for na área do fundo (modal)
+        modal.style.display = 'none'; // Fecha o modal
     }
+};
