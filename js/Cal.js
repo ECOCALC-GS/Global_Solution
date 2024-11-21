a5b2b863613d502511cb9b777458728313b87043
JonasKimio <jonaskimio1@hotmail.com>
2024-11-21 15:34:51 -0300

    Commit final

--- js/Cal.js
+++ js/Cal.js
@@ -67,99 +67,109 @@ function calcular() {
     let mensagem = "";
     if (resultadoFinal >= 15) {
         mensagem = `
-            <h3>Sua pegada de carbono é Alta.</h3>
-            <p>Isso significa que suas atividades diárias estão gerando uma quantidade significativa de emissões de CO₂ na atmosfera, o que contribui para o aquecimento global e mudanças climáticas. Atividades como o uso excessivo de energia de fontes não-renováveis (como carvão ou gás), o transporte individual com veículos movidos a combustíveis fósseis e o consumo de carne em grande quantidade são os principais responsáveis por essa pegada.</p>
-            <h4>Implicações Globais:</h4>
-            <ul>
-                <li><b>Se todos no Brasil adotassem o seu estilo de vida,</b> as emissões de CO₂ seriam consideravelmente aumentadas, acelerando o impacto negativo sobre o clima no país.</li>
-                <li><b>Se todos no mundo seguissem o seu exemplo,</b> as emissões de CO₂ aumentariam exponencialmente, exacerbando ainda mais os efeitos do aquecimento global e acelerando a mudança climática.</li>
-            </ul>
-            <h4>Recomendações para Reduzir sua Pegada de Carbono:</h4>
-            <ul>
-                <li>Reduza o consumo de energia proveniente de fontes fósseis. Considere instalar painéis solares ou mudar para fornecedores de energia renovável.</li>
-                <li>Considere alternativas de transporte mais sustentáveis, como andar de bicicleta, caminhar ou utilizar transporte público.</li>
-                <li>Adote uma dieta com menor impacto ambiental, reduzindo o consumo de carne vermelha e priorizando alimentos vegetais.</li>
-            </ul>
-            <h4>Como a Pegada de Carbono Afeta o Meio Ambiente?</h4> 
-            <ul>
-                <li>
-                    <b>Mudanças Climáticas:</b> A principal consequência de uma pegada de carbono alta é o agravamento das mudanças 
-                    climáticas. Isso inclui o aumento da temperatura global, eventos climáticos extremos (como secas, tempestades e inundações), 
-                    derretimento de geleiras, aumento do nível do mar e mudanças nos ecossistemas. 
-                </li>
-                <li>
-                    <b>Qualidade do Ar e Saúde:</b> A queima de combustíveis fósseis para gerar energia e transporte contribui para a poluição do ar, 
-                    que é responsável por problemas respiratórios e outras doenças relacionadas à qualidade do ar, afetando a saúde humana e animal.
-                </li>
-                <li>
-                    <b>Perda de Biodiversidade:</b> O aumento da temperatura e a destruição de habitats naturais devido ao desmatamento, mudanças no 
-                    uso da terra e poluição causam uma perda significativa de biodiversidade, o que compromete a estabilidade dos ecossistemas. 
-                </li>
-            </ul>
+<h3>Sua pegada de carbono é alta</h3>
+
+<p>O que indica que suas atividades diárias estão gerando uma quantidade significativa de emissões de CO₂ na atmosfera. 
+Isso contribui para o aquecimento global e as mudanças climáticas. Aspectos como o uso elevado de energia em casa, 
+aquecimento de água com gás, consumo excessivo de combustível para veículos, alta ingestão de alimentos de origem animal, 
+compras internacionais frequentes e viagens aéreas contribuem substancialmente para esse resultado.</p>
+
+<h4>Implicações Globais:</h4>
+
+<p>No cenário global, com uma população de 8 bilhões, emissões tão altas por pessoa resultariam em um aumento acelerado das temperaturas médias, 
+levando ao derretimento de geleiras, elevação do nível do mar, desastres naturais mais intensos e eventos climáticos imprevisíveis. </p> 
+
+<h4>Como a Pegada de Carbono Afeta o Meio Ambiente?</h4> 
+<ul>
+<li><b>Mudanças Climáticas:</b> A principal consequência de uma pegada de carbono alta é o agravamento das <b>mudanças climáticas. </b>
+Isso inclui o aumento da temperatura global, eventos climáticos extremos (como secas, tempestades e inundações), derretimento de geleiras, 
+aumento do nível do mar e mudanças nos ecossistemas. </li>
+
+<li><b>Qualidade do Ar e Saúde:</b> A queima de combustíveis fósseis para gerar energia e transporte contribui para a poluição do ar, 
+que é responsável por problemas respiratórios e outras doenças relacionadas à qualidade do ar, afetando a saúde humana e animal. </li>
+
+<li><b>Perda de Biodiversidade:</b> O aumento da temperatura e a destruição de habitats naturais devido ao desmatamento, 
+mudanças no uso da terra e poluição causam uma perda significativa de biodiversidade, o que compromete a estabilidade dos ecossistemas. </li>
+</ul>
+
+<h4>Conclusão</h4> 
+
+<p>Seu comportamento e suas escolhas diárias têm um impacto direto no meio ambiente. 
+Cada pequeno ajuste que você faz em sua rotina pode contribuir para um planeta mais saudável e sustentável para as gerações futuras. 
+A boa notícia é que <b>todos podemos fazer a diferença.</b> Mesmo que suas emissões ainda sejam altas ou médias, o importante é <b>começar a mudança agora.</b> 
+Ao adotar hábitos mais sustentáveis, você não só reduz a sua pegada de carbono, mas também inspira outros a seguir seu exemplo, criando um efeito 
+positivo que pode se espalhar em escala global.</p>
         `;
     } else if (resultadoFinal >= 5 && resultadoFinal < 15) {
         mensagem = `
-            <h3>Sua pegada de carbono é Média.</h3>
-            <p>Isso significa que você está tomando algumas decisões que ajudam a reduzir o impacto ambiental, mas ainda há espaço para melhorar. O uso de energia de fontes renováveis, o transporte público e uma dieta equilibrada são bons passos, mas mudanças adicionais podem contribuir para uma pegada de carbono mais baixa.</p>
-            <h4>Implicações Globais:</h4>
-            <ul>
-                <li><b>Se todos no Brasil adotassem o seu estilo de vida,</b> as emissões de CO₂ seriam significativamente menores do que no cenário anterior, mas ainda assim poderiam contribuir para o aumento das emissões nacionais e globais.</li>
-                <li><b>Se todos no mundo seguissem o seu exemplo,</b> ainda assim as emissões seriam consideráveis, mas com um impacto relativamente mais moderado em comparação com uma pegada de carbono alta. Mesmo uma pegada média, se adotada em escala global, continuaria a contribuir para o aquecimento global, embora de forma menos intensa.</li>
-            </ul>
-            <h4>Recomendações para Reduzir sua Pegada de Carbono:</h4>
-            <ul>
-                <li>Aumente a utilização de transporte público ou compartilhe caronas para reduzir as emissões associadas ao transporte. </li>
-                <li>Se possível, adote hábitos de consumo mais sustentáveis, como reciclar mais, reduzir o desperdício de alimentos e utilizar produtos com menor pegada ambiental.</li>
-                <li>Considere reduzir ainda mais o consumo de carne, especialmente a carne vermelha, e substitua por opções vegetais. </li>
-            </ul>
-            <h4>Como a Pegada de Carbono Afeta o Meio Ambiente?</h4> 
-            <ul>
-                <li>
-                    <b>Mudanças Climáticas:</b> A principal consequência de uma pegada de carbono alta é o agravamento das mudanças 
-                    climáticas. Isso inclui o aumento da temperatura global, eventos climáticos extremos (como secas, tempestades e inundações), 
-                    derretimento de geleiras, aumento do nível do mar e mudanças nos ecossistemas. 
-                </li>
-                <li>
-                    <b>Qualidade do Ar e Saúde:</b> A queima de combustíveis fósseis para gerar energia e transporte contribui para a poluição do ar, 
-                    que é responsável por problemas respiratórios e outras doenças relacionadas à qualidade do ar, afetando a saúde humana e animal.
-                </li>
-                <li>
-                    <b>Perda de Biodiversidade:</b> O aumento da temperatura e a destruição de habitats naturais devido ao desmatamento, mudanças no 
-                    uso da terra e poluição causam uma perda significativa de biodiversidade, o que compromete a estabilidade dos ecossistemas. 
-                </li>
-            </ul>
+<h3>Sua pegada de carbono é considerada média</h3>
+
+<p>Indicando que você já toma algumas ações positivas para reduzir o impacto ambiental. 
+No entanto, ainda há oportunidades de melhora. O uso de transporte público, fontes de energia 
+renováveis e uma dieta mais consciente são passos na direção certa, mas ajustes adicionais podem ajudar 
+a reduzir ainda mais suas emissões de CO₂. </p>
+
+<h4>Implicações Globais: </h4>
+
+<p>Se todos adotassem seu estilo de vida, o impacto climático seria menos intenso em comparação com uma pegada alta. 
+Ainda assim, as emissões globais continuariam contribuindo para o aquecimento global, resultando em mudanças climáticas, como eventos climáticos extremos e aumento do nível do mar. 
+Uma redução generalizada de pegadas médias ajudaria a desacelerar esses efeitos. </p>
+
+<h4>Como a Pegada de Carbono Afeta o Meio Ambiente?</h4> 
+<ul>
+<li><b>Mudanças Climáticas:</b> A principal consequência de uma pegada de carbono alta é o agravamento das <b>mudanças climáticas. </b>
+Isso inclui o aumento da temperatura global, eventos climáticos extremos (como secas, tempestades e inundações), derretimento de geleiras, 
+aumento do nível do mar e mudanças nos ecossistemas. </li>
+
+<li><b>Qualidade do Ar e Saúde:</b> A queima de combustíveis fósseis para gerar energia e transporte contribui para a poluição do ar, 
+que é responsável por problemas respiratórios e outras doenças relacionadas à qualidade do ar, afetando a saúde humana e animal. </li>
+
+<li><b>Perda de Biodiversidade:</b> O aumento da temperatura e a destruição de habitats naturais devido ao desmatamento, 
+mudanças no uso da terra e poluição causam uma perda significativa de biodiversidade, o que compromete a estabilidade dos ecossistemas. </li>
+</ul>
+
+<h4>Conclusão</h4> 
+
+<p>Seu comportamento e suas escolhas diárias têm um impacto direto no meio ambiente. 
+Cada pequeno ajuste que você faz em sua rotina pode contribuir para um planeta mais saudável e sustentável para as gerações futuras. 
+A boa notícia é que <b>todos podemos fazer a diferença.</b> Mesmo que suas emissões ainda sejam altas ou médias, o importante é <b>começar a mudança agora.</b> 
+Ao adotar hábitos mais sustentáveis, você não só reduz a sua pegada de carbono, mas também inspira outros a seguir seu exemplo, criando um efeito 
+positivo que pode se espalhar em escala global.</p> 
         `;
     } else if (resultadoFinal < 5) {
         mensagem = `
-            <h3>Sua pegada de carbono é Baixa.</h3>
-            <p>Isso significa que você está fazendo escolhas conscientes que minimizam as emissões de CO₂. Seu consumo de energia renovável, o uso de transporte sustentável, a dieta equilibrada e os hábitos de consumo consciente estão ajudando a reduzir seu impacto ambiental.</p>
-            <h4>Implicações Globais:</h4>
-            <ul>
-                <li><b>Se todos no Brasil adotassem o seu estilo de vida,</b> o país poderia reduzir substancialmente suas emissões de gases de efeito estufa, ajudando a alcançar as metas climáticas nacionais e contribuindo para a mitigação das mudanças climáticas. </li>
-                <li><b>Se todos no mundo seguissem o seu exemplo,</b> isso resultaria em uma grande redução nas emissões globais de CO₂, ajudando a estabilizar o clima e a reduzir o aquecimento global. A adesão a hábitos de baixo carbono poderia, em longo prazo, reverter alguns dos efeitos mais severos das mudanças climáticas. </li>
-            </ul>
-            <h4>Recomendações para Reduzir sua Pegada de Carbono:</h4>
-            <ul>
-                <li>Continue explorando formas de otimizar ainda mais o uso de energia em sua casa (por exemplo, utilizando eletrodomésticos eficientes e adotando práticas como o uso de luz natural). </li>
-                <li>Incentive outras pessoas a seguir exemplos de consumo consciente, como reduzir o uso de plásticos, priorizar transporte sustentável e adotar dietas com menos carne. </li>
-                <li>Considere se envolver em iniciativas locais e globais para promover a sustentabilidade, como plantar árvores, apoiar políticas públicas de redução de emissões e apoiar empresas com práticas sustentáveis. </li>
-            </ul>
-            <h4>Como a Pegada de Carbono Afeta o Meio Ambiente?</h4> 
-            <ul>
-                <li>
-                    <b>Mudanças Climáticas:</b> A principal consequência de uma pegada de carbono alta é o agravamento das mudanças 
-                    climáticas. Isso inclui o aumento da temperatura global, eventos climáticos extremos (como secas, tempestades e inundações), 
-                    derretimento de geleiras, aumento do nível do mar e mudanças nos ecossistemas. 
-                </li>
-                <li>
-                    <b>Qualidade do Ar e Saúde:</b> A queima de combustíveis fósseis para gerar energia e transporte contribui para a poluição do ar, 
-                    que é responsável por problemas respiratórios e outras doenças relacionadas à qualidade do ar, afetando a saúde humana e animal.
-                </li>
-                <li>
-                    <b>Perda de Biodiversidade:</b> O aumento da temperatura e a destruição de habitats naturais devido ao desmatamento, mudanças no 
-                    uso da terra e poluição causam uma perda significativa de biodiversidade, o que compromete a estabilidade dos ecossistemas. 
-                </li>
-            </ul>
+<h3>Parabéns! Sua pegada de carbono é considerada baixa</h3>
+
+<p>Refletindo escolhas conscientes e sustentáveis que ajudam a minimizar as emissões de CO₂. 
+Práticas como o uso de energia renovável, transporte sustentável, uma dieta equilibrada e hábitos de consumo 
+responsáveis estão contribuindo significativamente para a redução do seu impacto ambiental. </p>
+
+<h4>Implicações Globais:</h4> 
+
+<p>Se o estilo de vida de todos fosse semelhante ao seu, haveria uma redução substancial nas emissões globais de CO₂, 
+promovendo a estabilização do clima e desacelerando o aquecimento global. Esse exemplo de baixo carbono, adotado em 
+escala global, poderia ajudar a mitigar os impactos mais graves das mudanças climáticas e contribuir para um futuro mais sustentável.</p>
+
+<h4>Como a Pegada de Carbono Afeta o Meio Ambiente?</h4> 
+<ul>
+<li><b>Mudanças Climáticas:</b> A principal consequência de uma pegada de carbono alta é o agravamento das <b>mudanças climáticas. </b>
+Isso inclui o aumento da temperatura global, eventos climáticos extremos (como secas, tempestades e inundações), derretimento de geleiras, 
+aumento do nível do mar e mudanças nos ecossistemas. </li>
+
+<li><b>Qualidade do Ar e Saúde:</b> A queima de combustíveis fósseis para gerar energia e transporte contribui para a poluição do ar, 
+que é responsável por problemas respiratórios e outras doenças relacionadas à qualidade do ar, afetando a saúde humana e animal. </li>
+
+<li><b>Perda de Biodiversidade:</b> O aumento da temperatura e a destruição de habitats naturais devido ao desmatamento, 
+mudanças no uso da terra e poluição causam uma perda significativa de biodiversidade, o que compromete a estabilidade dos ecossistemas. </li>
+</ul>
+
+<h4>Conclusão</h4> 
+
+<p>Seu comportamento e suas escolhas diárias têm um impacto direto no meio ambiente. 
+Cada pequeno ajuste que você faz em sua rotina pode contribuir para um planeta mais saudável e sustentável para as gerações futuras. 
+A boa notícia é que <b>todos podemos fazer a diferença.</b> Mesmo que suas emissões ainda sejam altas ou médias, o importante é <b>começar a mudança agora.</b> 
+Ao adotar hábitos mais sustentáveis, você não só reduz a sua pegada de carbono, mas também inspira outros a seguir seu exemplo, criando um efeito 
+positivo que pode se espalhar em escala global.</p> 
         `;
     }
 
